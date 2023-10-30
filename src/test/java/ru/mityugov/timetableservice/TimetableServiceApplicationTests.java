fec4e4784fbc45ccced90f4f0e61e32abcf7a0ae
Mityugov Konstantin <alxmira@yandex.ru>
2023-10-30 12:07:28 +0700

    Init commit

---
+++ src/test/java/ru/mityugov/timetableservice/TimetableServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mityugov.timetableservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
